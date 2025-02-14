cfefafca966f6348194da86d04f7ac64e6342c62
glacks06 <a91125149@gmail.com>
2025-02-14 11:51:16 +0900

    25.02.14_11.50am

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-class Board extends React.Component {
-    renderSquare(i) {
-      return <Square value={i} />;
-    }
-}
+import App from "./App";
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {this.props.value}
-        </button>
-      );
-    }
-}
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
